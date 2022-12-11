c462b1e8668979ba7280567b7a14c8eaa7ba788b
chomade <choyoonh2008@gmail.com>
2022-12-11 10:33:54 +0900

    1st day

--- main.py
+++ main.py
@@ -5,35 +5,36 @@ import os
 import time
 import json
 
-
 pygame.init()
-
-font = pygame.font.SysFont('arial', 30)
 
 screenwidth = 800
 screenheight = 450
 
 white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
 black = (0, 0, 0)
 
+events = pygame.event.get()
+
 mouse = pygame.mouse.get_pos()
-click = pygame.mouse.get_pressed()
 
 mainbuttonheight = screenheight / 7
+intmainbuttonheight = int(mainbuttonheight)
 mainbuttonx = screenwidth / 10
 mainbuttonwidth = screenwidth - mainbuttonx * 2
 
 setting = False
 main_screen = True
 
-def quitgame():
-    Quit = True
+font = pygame.font.Font("data\DNF.otf", intmainbuttonheight)
+
 
 def resolutionscreen():
+    global main_screen
+    global setting
     resolution = True
     main_screen = False
+    setting = False
+
 
 screen = pygame.display.set_mode((screenwidth, screenheight))
 
@@ -58,64 +59,74 @@ class Button():
 
 main_buttons = [
     Button("play", mainbuttonx, mainbuttonheight * 1, mainbuttonwidth, mainbuttonheight, 1),
-    Button("settings", mainbuttonx, mainbuttonheight * 3, mainbuttonwidth, mainbuttonheight, 3),
-    Button("exit", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
+    Button("settings", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
 ]
 
 setting_buttons = [
     Button("screen resolution", mainbuttonx, mainbuttonheight * 1, mainbuttonwidth, mainbuttonheight, 1),
     Button("sound", mainbuttonx, mainbuttonheight * 3, mainbuttonwidth, mainbuttonheight, 3),
-    Button("exit", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
+    Button("back", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
 ]
 
 
-
-
-while main_screen:
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    for button in main_buttons:
-        pygame.draw.rect(screen, white, button.rect)
-        textsurface = font.render(button.name, True, black)
-        screen.blit(textsurface, (mainbuttonx, mainbuttonheight * button.number))
-        if button.x + button.width >= mouse[0] >= button.x and button.y + button.height >= mouse[1] >= button.y and click[0]:
-            if mainbuttonheight * 1 <= button.y <= mainbuttonheight * 2:
-                print("1")
-            elif mainbuttonheight * 3 <= button.y <= mainbuttonheight * 4:
-                main_screen = False
-                setting = True
-            elif mainbuttonheight * 5 <= button.y <= mainbuttonheight * 6:
+def maingame():
+    global main_screen
+    global setting
+    while main_screen:
+        mouse = pygame.mouse.get_pos()
+        for button, event in main_buttons, events:
+            pygame.draw.rect(screen, white, button.rect)
+            textsurface = font.render(button.name, True, black)
+            screen.blit(textsurface, (mainbuttonx, mainbuttonheight * button.number))
+            if button.x + button.width >= mouse[0] >= button.x and button.y + button.height >= mouse[1] >= button.y and \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                if mainbuttonheight * 1 <= button.y <= mainbuttonheight * 2:
+                    print("1")
+                elif mainbuttonheight * 5 <= button.y <= mainbuttonheight * 6:
+                    screen.fill(black)
+                    main_screen = False
+                    setting = True
+                    settinggame()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
                 exit()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-            quit()
-    pygame.display.update()
+                quit()
+        pygame.display.update()
 
-while setting:
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    for button in setting_buttons:
-        pygame.draw.rect(screen, white, button.rect)
-        if button.x + button.width >= mouse[0] >= button.x and button.y + button.height >= mouse[1] >= button.y and click[0]:
-            if mainbuttonheight * 1 <= button.y <= mainbuttonheight * 2:
-                print("1")
-            elif mainbuttonheight * 3 <= button.y <= mainbuttonheight * 4:
-                main_screen = False
-                setting = True
-            elif mainbuttonheight * 5 <= button.y <= mainbuttonheight * 6:
+
+def settinggame():
+    global setting
+    global main_screen
+    while setting:
+        pygame.display.update()
+        mouse = pygame.mouse.get_pos()
+        click = pygame.MOUSEBUTTONUP
+        for button in setting_buttons:
+            screen.fill(black)
+            pygame.draw.rect(screen, white, button.rect)
+            textsurface = font.render(button.name, True, black)
+            screen.blit(textsurface, (mainbuttonx, mainbuttonheight * button.number))
+            if button.x + button.width >= mouse[0] >= button.x and button.y + button.height >= mouse[1] >= button.y and \
+                    click[0]:
+                if mainbuttonheight * 1 <= button.y <= mainbuttonheight * 2:
+                    print("1")
+                elif mainbuttonheight * 3 <= button.y <= mainbuttonheight * 4:
+                    main_screen = False
+                    setting = True
+                elif mainbuttonheight * 5 <= button.y <= mainbuttonheight * 6:
+                    screen.fill(black)
+                    setting = False
+                    main_screen = True
+                    maingame()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
                 exit()
+                quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-            quit()
+
+maingame()
 
 with open("data\key.json", "w") as outfile:
     json.dump(key, outfile)
@@ -127,7 +138,7 @@ class Key():
         self.y = y
         self.color = color
 
+
 keys = [
     Key(100, 0, (0, 0, 0))
 ]
-
