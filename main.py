dec52e02f1a3a5af6a49d66a1ba05e6399ce0980
chomade <choyoonh2008@gmail.com>
2022-12-12 00:08:52 +0900

    added scenes

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ import json
 
 pygame.init()
 
-screenwidth = 800
-screenheight = 450
+screenwidth = 960
+screenheight = 540
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 events = pygame.event.get()
 
-mouse = pygame.mouse.get_pos()
+displaynumber = 0
+displaysize = pygame.display.list_modes()[int(displaynumber)]
 
+mouse = pygame.mouse.get_pos()
+pygame.mouse.set_visible(False)
 mainbuttonheight = screenheight / 7
-intmainbuttonheight = int(mainbuttonheight)
+mainfontsize = int(mainbuttonheight * 0.9)
 mainbuttonx = screenwidth / 10
 mainbuttonwidth = screenwidth - mainbuttonx * 2
 bigbuttonheight = mainbuttonheight * 1.3
-intbigbuttonheight = int(bigbuttonheight)
+bigfontsize = int(bigbuttonheight * 0.9)
 bigbuttonwidth = mainbuttonwidth * 1.15
 bigbuttonx = screenwidth / 2 - bigbuttonwidth / 2
 bigbuttonbasicy = mainbuttonheight * 1.5 - bigbuttonheight / 2
@@ -33,9 +36,10 @@ setting2 = False
 setting3 = False
 main_screen = True
 main_screen2 = False
+screensetting = False
 
-font = pygame.font.Font("data\DNF.otf", intmainbuttonheight)
-bigfont = pygame.font.Font("data\DNF.otf", intbigbuttonheight)
+font = pygame.font.Font("data\DNF.otf", mainfontsize)
+bigfont = pygame.font.Font("data\DNF.otf", bigfontsize)
 
 screen = pygame.display.set_mode((screenwidth, screenheight))
 
@@ -46,6 +50,14 @@ key = {
     "Fourth": pygame.K_k
 }
 
+
+
+
+class Key():
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
 
 class Button():
     def __init__(self, name, x, y, width, height, number):
@@ -69,17 +81,16 @@ setting_buttons = [
     Button("back", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
 ]
 
-
-class Key():
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-
+screen_buttons = [
+    Button('displaysize', mainbuttonx, mainbuttonheight * 1, mainbuttonwidth, mainbuttonheight, 1),
+    Button("full screen", mainbuttonx, mainbuttonheight * 3, mainbuttonwidth, mainbuttonheight, 3),
+    Button("back", mainbuttonx, mainbuttonheight * 5, mainbuttonwidth, mainbuttonheight, 5),
+]
 
 keys = [
     Key(100, 0, (0, 0, 0))
 ]
+
 
 with open("data\key.json", "w") as outfile:
     json.dump(key, outfile)
@@ -166,6 +177,7 @@ def settinggame():
     global setting3
     global main_screen
     global main_screen2
+    global screensetting
     screen.fill(black)
     main_screen = False
     main_screen2 = False
@@ -194,8 +206,8 @@ def settinggame():
                 setting3 = True
                 settinggame3()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                main_screen2 = True
-                maingame2()
+                screensetting = True
+                screengame()
 
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -280,8 +292,8 @@ def settinggame3():
                 setting2 = True
                 settinggame2()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                main_screen2 = True
-                maingame2()
+                main_screen = True
+                maingame()
 
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -289,8 +301,83 @@ def settinggame3():
                 quit()
 
 
+def screengame():
+    global setting
+    global screensetting
+    global displaysize
+    global displaynumber
+    global screenheight
+    global screenwidth
+    global mainbuttonheight
+    global mainfontsize
+    global mainbuttonx
+    global mainbuttonwidth
+    global bigbuttonheight
+    global bigfontsize
+    global bigbuttonwidth
+    global bigbuttonx
+    global bigbuttonbasicy
+    screen.fill(black)
+    setting = False
+    enter = False
+    displaysize = pygame.display.list_modes()
+    displaylenth = len(displaysize) - 1
+    while screensetting:
+        for event in pygame.event.get():
+            for button in setting_buttons:
+                displaysize = str(pygame.display.list_modes()[displaynumber])
+                selectnum = 0
+                yplace = selectnum
+                bigbuttony = bigbuttonbasicy + mainbuttonheight * yplace
+                pygame.draw.rect(screen, white, button.rect)
+                textsurface = font.render(button.name, True, black)
+                screen.blit(textsurface, (mainbuttonx, mainbuttonheight * button.number))
+                firstsels = [button for button in setting_buttons if button.number == 1]
+                for firstsel in firstsels:
+                    pygame.draw.rect(screen, white, (bigbuttonx, bigbuttony, bigbuttonwidth, bigbuttonheight))
+                    bigtextsurface = bigfont.render(displaysize, True, black)
+                    screen.blit(bigtextsurface, (bigbuttonx, bigbuttony))
+                pygame.display.update()
+                print(displaylenth)
 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                setting2 = True
+                settinggame2()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                setting3 = True
+                settinggame3()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                enter = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                if displaynumber > 0:
+                    displaynumber = displaynumber - 1
+                    displaysize = str(pygame.display.list_modes()[displaynumber])
+                    for firstsel in firstsels:
+                        pygame.draw.rect(screen, white, (bigbuttonx, bigbuttony, bigbuttonwidth, bigbuttonheight))
+                        bigtextsurface = bigfont.render(displaysize, True, black)
+                        screen.blit(bigtextsurface, (bigbuttonx, bigbuttony))
+                        pygame.display.update()
+                        screenwidth = pygame.display.list_modes()[displaynumber][0]
+                        screenheight = pygame.display.list_modes()[displaynumber][1]
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                if displaynumber < displaylenth:
+                    displaynumber = displaynumber + 1
+                    displaysize = str(pygame.display.list_modes()[displaynumber])
+                    for firstsel in firstsels:
+                        pygame.draw.rect(screen, white, (bigbuttonx, bigbuttony, bigbuttonwidth, bigbuttonheight))
+                        bigtextsurface = bigfont.render(displaysize, True, black)
+                        screen.blit(bigtextsurface, (bigbuttonx, bigbuttony))
+                        pygame.display.update()
+                        screenwidth = int(pygame.display.list_modes()[displaynumber][0])
+                        screenheight = int(pygame.display.list_modes()[displaynumber][1])
+            if enter == True:
+                pygame.display.set_mode((screenwidth, screenheight))
 
+                enter = False
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+                quit()
 
 
 
